32df932303a210805bc09a8ff08e75104f6e2092
taylorallen0913 <taylorallen0913@gmail.com>
2022-05-08 02:43:51 -0700

    started working on landing page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Navbar: NextPage = () => {
   return (
     <div>
-      <nav className="bg-blue-800 dark:bg-black dark:text-white flex flex-col md:flex-row items-center md:justify-between px-6 py-4 border-b border-b-gray-60 shadow-sm mx-auto">
+      <nav className="bg-blue-800 dark:bg-black dark:text-white flex flex-col md:flex-row items-center md:justify-between px-6 py-4 border-b border-b-gray-60 shadow-sm max-w-screen-2xl mx-auto">
         <div>
           <h2 className="text-3xl font-bold">
             <a className="text-white">Studyzone</a>
@@ -13,27 +13,22 @@ const Navbar: NextPage = () => {
           <ul className="flex flex-col md:flex-row md:space-x-5 w-full items-center">
             <li>
               <a className="text-white font-semibold tracking-tight block cursor-pointer p-2 hover:text-blue-500 transition-colors duration-300">
-                Products
+                Find Tutors
               </a>
             </li>
             <li>
               <a className="text-white font-semibold tracking-tight block cursor-pointer p-2 hover:text-blue-500 transition-colors duration-300">
-                Company
-              </a>
-            </li>
-            <li>
-              <a className="text-white font-semibold tracking-tight block cursor-pointer p-2 hover:text-blue-500 transition-colors duration-300">
-                Partners
-              </a>
-            </li>
-            <li>
-              <a className="text-white font-semibold tracking-tight block cursor-pointer p-2 hover:text-blue-500 transition-colors duration-300">
-                News
+                Learn More
               </a>
             </li>
             <li>
               <a className="text-white font-semibold tracking-tight block cursor-pointer p-2 hover:text-blue-500 transition-colors duration-300">
                 Contact Us
+              </a>
+            </li>
+            <li>
+              <a className="text-white font-semibold tracking-tight block cursor-pointer p-2 hover:text-blue-500 transition-colors duration-300">
+                Blog
               </a>
             </li>
           </ul>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from 'next';
+import Navbar from '../components/Navbar';
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <div
+        className="bg-blue-800"
+        style={{ height: '40em', marginTop: '-100px' }}
+      />
+      <div style={{ width: '50%', paddingLeft: '10%' }}>
+        <h1 className="text-white text-5xl" style={{ marginTop: '-10em' }}>
+          Find the best tutors for classes you need help in
+        </h1>
+        <p className="text-gray-300 text-2xl mt-5">
+          Find the best tutors for classes you need help in
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
