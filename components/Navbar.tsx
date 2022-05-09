fdc90bf9e2813bf66bef061c8a6ff033df85d7ef
taylorallen0913 <taylorallen0913@gmail.com>
2022-05-08 18:19:23 -0700

    refractored landing page to tailwind

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Navbar: NextPage = () => {
   return (
     <div>
-      <nav className="bg-blue-800 dark:bg-black dark:text-white flex flex-col md:flex-row items-center md:justify-between px-6 py-4 border-b border-b-gray-60 shadow-sm max-w-screen-2xl mx-auto">
+      <nav className="bg-blue-800 dark:bg-black dark:text-white flex flex-col md:flex-row items-center md:justify-between px-6 py-4 shadow-sm max-w-screen-2xl">
         <div>
           <h2 className="text-3xl font-bold">
             <a className="text-white">Studyzone</a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,21 @@ import Teacher from '../components/Teacher';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Navbar />
-      <div className="bg-blue-800" style={{ height: '45em', marginTop: '-5%' }}>
-        <div style={{ width: '50%', paddingLeft: '10%', paddingTop: '12%' }}>
-          <h1 className="text-white font-medium text-6xl">
-            Find the Best Tutors for Classes You Need Help In Most
-          </h1>
-          <p className="text-gray-300 font-light text-2xl mt-5">
-            I love black people
-          </p>
-          <div style={{ marginLeft: '120%', marginTop: '-40%' }}>
+    <div className="w-screen bg-blue-800 pb-20">
+      <div className="container mx-auto">
+        <div className="w-screen absolute">
+          <Navbar />
+        </div>
+        <div className="grid grid-cols-2 pt-44 ">
+          <div className="pl-5">
+            <h1 className="text-white font-medium text-6xl">
+              Find the Best Tutors for Classes You Need Help In Most
+            </h1>
+            <p className="text-gray-300 font-light text-2xl mt-5">
+              I love black people
+            </p>
+          </div>
+          <div className="ml-40">
             <Teacher />
           </div>
         </div>
